a967ef67a1878a7d4c8b71c80f1b18b7274e6c8a
FennEXE <luuknagtegaal@gmail.com>
2022-04-07 08:52:04 +0200

    Forgot to push

---
+++ app/Http/Controllers/groceryController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class groceryController extends Controller
+{
+    public function index()
+    {
+        $grocery = grocery::All();
+        return response()->json($grocery);
+    }
+
+    public function create()
+    {
+        $this->authorize('create', grocery::class);
+    }
+
+    public function store(grocery $grocery)
+    {
+        $validated = $grocery->validate();
+
+        $grocery = grocery::create([
+			'name' => $validated['name'],
+			'price' => $validated['price'],
+			'amount' => $validated['amount'],
+			'max_amount' => $validated['max_amount'],		
+		]);
+
+        return response()->json(grocery::All());
+    }
+
+    public function edit(grocery $grocery)
+    {
+        //
+    }
+
+    public function update(grocery $grocery)
+    {
+        //I forgot how to do this part
+        $validated = $grocery->validate();
+        
+        $grocery = grocery::create([
+			'name' => $validated['name'],
+			'price' => $validated['price'],
+			'amount' => $validated['amount'],
+			'max_amount' => $validated['max_amount'],		
+		]);
+    }
+
+    public function destroy(grocery $grocery)
+    {
+		$product->delete();
+
+        return response()->json(grocery::All());
+    }
+}
---
+++ database/migrations/2022_04_05_062558_grocery.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Grocery extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('price', $precision = 8, $scale = 2);
+            $table->integer('amount');
+            $table->integer('max_amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
