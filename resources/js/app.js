c1151c1c6755b721f336e51f4a86f21e78125007
FennEXE <luuknagtegaal@gmail.com>
2022-04-21 10:29:11 +0200

    Breeze installed

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,19 @@
+
 import Vue from "vue";
+
+//Alpine
+require('./bootstrap');
+import Alpine from 'alpinejs';
+window.Alpine = Alpine;
+Alpine.start();
 
 //Main pages
 import App from "./App.vue";
 
 //Import store
 import store from "./store";
+
+
 
 //Import router
 import router from "./router";
---
+++ resources/js/components/blog.vue
@@ -0,0 +1,67 @@
+<template>
+    <div>
+		<table id="groceryTable">
+			<tr>
+				<th>Posts</th>
+				<th>Upvotes</th>
+				<th>Downvotes</th>
+			</tr>
+		</table>	
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Blog",
+	data() {
+		return {
+			
+		}
+	},
+	methods: {
+		
+	},
+	computed: {
+		
+	},
+	created() {
+		this.$store.dispatch('getAllBlogposts');
+	}	
+};
+</script>
+
+<style scoped>
+th{
+	min-width: 100px;
+}
+div{
+	padding-top: 20px;
+	padding-bottom: 40px;
+}
+h3 {
+	margin: 40px 0 0;
+}
+ul {
+	list-style-type: none;
+	padding: 0;
+}
+li {
+	display: inline-block;
+	margin: 0 10px;
+}
+a {
+	color: #42b983;
+}
+input {
+	background-color: rgb(65, 65, 65);
+	color: #a2bdd8;
+	font-size: 0.8em;
+	border-radius: 5px;
+	text-align: center;
+	width: 150px;
+}
+#groceryTable {
+	margin: 0 auto;
+	font-size: 2em;
+}
+</style>
