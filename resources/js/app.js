8b108990505a7f8157c2f6c392fccbb83cec8fed
FennEXE <luuknagtegaal@gmail.com>
2022-07-01 12:55:15 +0200

    .

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,12 @@
 
 import Vue from "vue";
 
+
+import axios from 'axios';
+window.axios = axios;
+
 //Alpine
-// require('./bootstrap');
+//require('./bootstrap');
 // import Alpine from 'alpinejs';
 // window.Alpine = Alpine;
 // Alpine.start();
